b5862dba95d2919c8749100a4ebb4414e269ac2c
Arun Rajora <rajora.rajora2@gmail.com>
2021-12-21 20:35:45 +0530

    Add mui and navbar.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
 import { loadInitialData } from './store/actions';
 
 function App() {
@@ -8,7 +11,22 @@ function App() {
   useEffect(() => {
     dispatch(loadInitialData());
   }, [dispatch]);
-  return <div className='App'>Employee poll</div>;
+  return (
+    <div className='App'>
+      <NavBar />
+      <Routes>
+        <Route index element={<div>Home</div>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/add' element={<div>Add</div>} />
+        <Route path='/leaderboard' element={<div>Leader</div>} />
+        <Route
+          path='/questions/:question_id'
+          element={<div>Question Details</div>}
+        />
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,119 @@
+import { AppBar } from '@mui/material';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const pages = [
+  { name: 'Home', path: '/' },
+  { name: 'Add', path: '/add' },
+  { name: 'Leaderboard', path: '/leaderboard' },
+];
+
+function NavBar({ a }) {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            Employee Poll
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map(({ name, path }) => (
+                <MenuItem
+                  key={path}
+                  onClick={handleCloseNavMenu}
+                  component={Link}
+                  to={path}
+                >
+                  <Typography textAlign='center'>{name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            Employee Poll
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map(({ name, path }) => (
+              <Button
+                key={path}
+                component={Link}
+                to={path}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {name}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Avatar alt='User Pic' src='/static/images/something.jpg' />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar;
